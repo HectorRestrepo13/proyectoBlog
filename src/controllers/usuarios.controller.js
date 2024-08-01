d4d5031e33050896524fe8dd4d79fdf0d03e657d
Hector restrepo <hector.soto20034@gmail.com>
2024-08-01 10:00:51 -0500

    modificacion con el multer para que pueda editar la foto de perfil

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -336,3 +336,109 @@ export const func_iniciarSesion = async (req, res) => {
 
 
 // -- FIN FUNCION --
+
+// FUNCION PARA EDITAR USUARIO
+
+export const func_editarPerfil = async (req, res) => {
+
+    try {
+
+        const { nombre, apellido, correo, telefono, id } = req.query;
+
+        // Aquí llamamos al middleware de Multer directamente
+        upload.single('foto')(req, res, async function (err) {
+            if (err instanceof multer.MulterError) {
+                res.status(200).send({
+                    status: false,
+                    descripcion: "No se pudo Insertar los datos Verificar LA parte del multer",
+                    data: null,
+                    error: err
+                })
+            } else if (err) {
+                res.status(200).send({
+                    status: false,
+                    descripcion: "No se pudo Insertar los datos Verificar LA parte del multer",
+                    data: null,
+                    error: err
+                })
+            }
+            console.log("ACA voy a mirar el Archivo :" + req.file);
+            // aca voy a verificar que sea imagen 
+            let archivo = req.file.mimetype.split("/");
+            let type = archivo[1];
+            if (type.toUpperCase() == "JPEG" || type.toUpperCase() == "PNG") {
+
+
+                const [updated] = await usuario.update(
+                    { NombreUsuario: nombre, ApellidoUsuario: apellido, TelefonoUsurio: telefono, CorreoUsuario: correo, ImagenUsuario: req.file.filename },
+                    {
+                        where: {
+                            CedulaUsuario: id,
+                        },
+
+                    },
+                );
+
+                if (updated) {
+                    const updatedUsuario = await usuario.findOne({ where: { CedulaUsuario: id } });
+                    return res.status(200).send({
+                        status: true,
+                        descripcion: "Perfil editado con éxito",
+                        data: updatedUsuario,
+                        error: null
+                    });
+                } else {
+                    fs.unlinkSync(req.file.path);
+
+                    return res.status(200).send({
+                        status: false,
+                        descripcion: "Usuario no encontrado",
+                        data: null,
+                        error: "No se pudo encontrar o actualizar el usuario"
+                    });
+                }
+
+            }
+            else {
+
+                fs.unlinkSync(req.file.path);
+                console.log("Archivo eliminado correctamente");
+                res.status(200).send({
+                    status: false,
+                    descripcion: "Solo se Aceptan Imagenes como PNG JPEG",
+                    data: null,
+                    error: null
+                })
+
+            }
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } catch (error) {
+
+        res.status(200).send({
+            status: false,
+            descripcion: "Hubo un error en la API",
+            data: null,
+            error: error.message
+        })
+    }
+
+}
+
+// -- FIN FUNCION --
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { func_registrarUsuario, pruebas, func_iniciarSesion } from "../controllers/usuarios.controller.js";
+import { func_registrarUsuario, pruebas, func_iniciarSesion, func_editarPerfil } from "../controllers/usuarios.controller.js";
 
 let rutaUsuario = Router();
 rutaUsuario.post("/usuario/registrarUsuario/", func_registrarUsuario)
 rutaUsuario.get("/usuario/iniciarSesion/", func_iniciarSesion)
 rutaUsuario.get("/usuario/pruebas/", pruebas)
+rutaUsuario.put("/usuario/editarPerfil/", func_editarPerfil)
+
 
 
 export default rutaUsuario;
